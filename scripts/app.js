9b4e3a04bbd50fb04de2e408657678a53655f603
mrogach2350 <m.rogach@gmail.com>
2016-08-24 09:33:03 -0700

    fixes giffaw

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,25 @@
 $(document).on("ready", function(){
-
+  $('form').on('submit', function(event){
+    event.preventDefault();
+    $.ajax({
+        method:'GET',
+        url: "http://api.giphy.com/v1/gifs/search?",
+        data: $("form").serialize().concat('&api_key=dc6zaTOxFJmzC'),
+        success: displayGifs
+    });
+  });
+  $('#reset').on('click', function handleEvent(event){
+    $('img').remove();
+  });
+  $('#add25').on('click', function handleEvent(event){
+    $('.gif-gallery').prepend('<img src="'+ json.data[i].images.downsized.url +'"/>' )
+  });
 });
+
+function displayGifs(json){
+  console.log(json);
+  for (var i = 0; i < json.data.length; i++){
+    console.log(json.data[i].url);
+    $('.gif-gallery').append('<img src="'+ json.data[i].images.downsized.url +'"/>' )
+  }
+}
